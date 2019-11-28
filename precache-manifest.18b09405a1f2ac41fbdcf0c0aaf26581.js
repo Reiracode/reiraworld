8453ec9367210e8faf28522cecf438caac1d9e48
reira <reira.igg@gmail.com>
2019-11-28 18:58:00 +0800

    Updates

---
+++ precache-manifest.18b09405a1f2ac41fbdcf0c0aaf26581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e559d76d07a074703aaba398ffa3a03",
+    "url": "/reiraworld/index.html"
+  },
+  {
+    "revision": "ae8b8799cbdcebc4b279",
+    "url": "/reiraworld/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a2ae703c7e0e2109134e",
+    "url": "/reiraworld/static/js/2.69e194e4.chunk.js"
+  },
+  {
+    "revision": "ae8b8799cbdcebc4b279",
+    "url": "/reiraworld/static/js/main.27aab8ed.chunk.js"
+  },
+  {
+    "revision": "0b3475a2d96f65472661",
+    "url": "/reiraworld/static/js/runtime-main.f43f5d62.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reiraworld/static/media/logo.25bf045c.svg"
+  }
+]);
