6bc887e475914b990501ad16b209e65c5caa14be
reira <reira.igg@gmail.com>
2019-11-29 11:15:58 +0800

    route_page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,159 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-// import logo from './logo.svg';
-import Button from "@material-ui/core/Button";
-import './App.css';
-import Menu from "./Menu"; // <= 作成したメニュー画面を読み込み
+// import React, { Component } from "react";
+// import { BrowserRouter, Route } from "react-router-dom";
+// // import logo from './logo.svg';
+// import Button from "@material-ui/core/Button";
+// import './App.css';
+// import Menu from "./Menu"; // <= 作成したメニュー画面を読み込み
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Menu/> 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Menu/> 
          
+//       </div>
+//     );
+//   }
+// }
+
+//   export default App;
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+
+// import { Avatar } from "@material-ui/core";
+
+export default function App() {
+  
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="./">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">gh-pages-demo</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/topics">
+            <Topics />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-  export default App;
+function Home(props) {
+  // const { classes } = props;
+  const styles = {
+    drawer: "drawer",
+    drawerPaper: "drawerPaper"
+  };
+  return (
+    <div>
+      <h2>Home</h2>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Card Subtitle
+          </Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <a
+        href="https://reiracode.github.io/react-gh-pages-demo/"
+        target="_blank"
+      >
+        DEMO
+      </a>
+
+      <a href="https://codepen.io/ReiraWorld/full/KeLexZ">
+        GSAP Text Animation
+      </a>
+
+      <iframe
+        src="https://codepen.io/ReiraWorld/full/KeLexZ"
+        width="250px"
+        height="300px"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+  );
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
--- src/menu.js
+++ src/menu.js
@@ -1,60 +1,53 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 
-const topPage = () => (
-  <div>
-    <h1>Top Page</h1>ここがトップページです
-  </div>
-);
-const page1 = () => (
-  <div>
-    <h1>page1</h1>1枚目のページです
-  </div>
-);
-const page2 = () => (
-  <div>
-    <h1>page2</h1>2枚目のページです
-  </div>
-);
-const page3 = () => (
-  <div>
-    <h1>page3</h1>3枚目のページです
-  </div>
-);
+const topPage = () => <div><h1>Top Page</h1>ここがトップページです</div>
+const page1 = () => <div><h1>page1</h1>1枚目のページです</div>
+const page2 = () => <div><h1>page2</h1>2枚目のページです</div>
+const page3 = () => <div><h1>page3</h1>3枚目のページです</div>
+const page404 = () => <div><h1>404</h1>存在しないページです</div>  //<= ヒットしなかった時用のページを追加
 
 const Menu = () => {
-  const liStyle = {
-    display: "inline",
-    width: "100px"
-  };
+    const liStyle = {
+        display: 'inline',
+        width: '100px'
+    }
 
-  return (
-    <Router>
-      <div style={{ width: "500px", textAlign: "left" }}>
-        <ul style={{ display: "flex" }}>
-          <li style={liStyle}>
-            <Link to="/">top</Link>
-          </li>
-          <li style={liStyle}>
-            <Link to="/page1">page1</Link>
-          </li>
-          <li style={liStyle}>
-            <Link to="/page2">page2</Link>
-          </li>
-          <li style={liStyle}>
-            <Link to="/page3">page3</Link>
-          </li>
-        </ul>
+    return (
+      <Router>
+        <div style={{ width: "500px", textAlign: "left" }}>
+          <ul style={{ display: "flex" }}>
+            <li style={liStyle}>
+              <Link to="/">top</Link>
+            </li>
+            <li style={liStyle}>
+              <Link to="/page1">page1</Link>
+            </li>
+            <li style={liStyle}>
+              <Link to="/reiraworld/page2">page2</Link>
+            </li>
+            <li style={liStyle}>
+              <Link to="/page3">page3</Link>
+            </li>
+          </ul>
 
-        <div style={{ marginLeft: "50px" }}>
-          <Route path="/" exact component={topPage} />
-          <Route path="/page1" exact component={page1} />
-          <Route path="/page2" exact component={page2} />
-          <Route path="/page3" exact component={page3} />
+          <div style={{ marginLeft: "50px" }}>
+            <Switch>
+            {/* <BrowserRouter> */}
+              <Route path="/" exact component={topPage} />
+              <Route path="/reiraworld/page1" exact component={page1} />
+              <Route path="/page2" exact component={page2} />
+              <Route path="/page3" exact component={page3} />
+              {/* <Route exact component={page404} /> */}
+              {/* //<= 一番末尾に追加 pathの指定も、対応するLinkの追加も必要ない */}
+
+              <Route exact component={topPage} />
+            {/* </BrowserRouter> */}
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
-  );
-};
+      </Router>
+    );
+}
 
-export default Menu;
+export default Menu
