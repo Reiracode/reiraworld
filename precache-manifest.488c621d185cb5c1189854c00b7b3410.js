494226b25fc700271e90e2bb192a1995d87f1cad
reira <reira.igg@gmail.com>
2019-11-29 08:01:27 +0800

    Updates

---
+++ precache-manifest.488c621d185cb5c1189854c00b7b3410.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87c9208c464ea4120c67b646072fcbc",
+    "url": "/reiraworld/index.html"
+  },
+  {
+    "revision": "1736da8d5d45ac4fc4d1",
+    "url": "/reiraworld/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4920a51c25f5bd262d60",
+    "url": "/reiraworld/static/js/2.01b84340.chunk.js"
+  },
+  {
+    "revision": "1736da8d5d45ac4fc4d1",
+    "url": "/reiraworld/static/js/main.0487273f.chunk.js"
+  },
+  {
+    "revision": "0b3475a2d96f65472661",
+    "url": "/reiraworld/static/js/runtime-main.f43f5d62.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reiraworld/precache-manifest.65d6ca14c2dd7e7eaad20d5fe38110a8.js"
+  "/reiraworld/precache-manifest.488c621d185cb5c1189854c00b7b3410.js"
 );
 
 self.addEventListener('message', (event) => {
