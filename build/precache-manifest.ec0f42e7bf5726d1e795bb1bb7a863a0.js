c3ca6f9d0b4b1a38662edf42cc315d9d4891896f
reira <reira.igg@gmail.com>
2020-01-14 18:10:02 +0800

    OK

---
+++ build/precache-manifest.ec0f42e7bf5726d1e795bb1bb7a863a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb5c4f332f28a683ea62605c7cb25c2",
+    "url": "/reiraworld/index.html"
+  },
+  {
+    "revision": "35aa57540cff673a04f6",
+    "url": "/reiraworld/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "440b1d5b3c1acd028c3b",
+    "url": "/reiraworld/static/css/main.5e420043.chunk.css"
+  },
+  {
+    "revision": "35aa57540cff673a04f6",
+    "url": "/reiraworld/static/js/2.b9a8f135.chunk.js"
+  },
+  {
+    "revision": "440b1d5b3c1acd028c3b",
+    "url": "/reiraworld/static/js/main.9c982ae3.chunk.js"
+  },
+  {
+    "revision": "0b3475a2d96f65472661",
+    "url": "/reiraworld/static/js/runtime-main.f43f5d62.js"
+  }
+]);
